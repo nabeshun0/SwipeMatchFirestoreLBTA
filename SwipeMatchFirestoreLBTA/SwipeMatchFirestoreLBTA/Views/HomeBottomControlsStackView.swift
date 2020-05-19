93d5b1a3ecda1c74ddaaef2c83ea2bcd9c4e5a1c
snt-nbzw <lapofyouth@gmail.com>
2020-05-20 01:22:11 +0900

    Rename HomeBottomControlsStackView

--- SwipeMatchFirestoreLBTA/SwipeMatchFirestoreLBTA/Views/HomeBottomControlsStackView.swift
+++ SwipeMatchFirestoreLBTA/SwipeMatchFirestoreLBTA/Views/HomeBottomControlsStackView.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class HomeBottomControlStackView: UIStackView {
+class HomeBottomControlsStackView: UIStackView {
 
     override init(frame: CGRect) {
         super.init(frame: frame)
