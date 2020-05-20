945a994b87632a5636e08456a134e3e4818c0708
snt-nbzw <lapofyouth@gmail.com>
2020-05-20 21:33:56 +0900

    Intro MVVM Card View Models #7

---
+++ SwipeMatchFirestoreLBTA/SwipeMatchFirestoreLBTA/ViewModels/CardViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  CardViewModel.swift
+//  SwipeMatchFirestoreLBTA
+//
+//  Created by member on 2020/05/20.
+//  Copyright © 2020 Shunta Nabezawa. All rights reserved.
+//
+
+import UIKit
+
+struct CardViewModel {
+    // we`ll define the properties that are view will display/render out
+    let imageName: String
+    let attributedString: NSAttributedString
+    let textAlignment: NSTextAlignment
+}
+
+// what exactly do we do with this card view model thing???
+
